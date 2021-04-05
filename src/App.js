68424f27c29966230830ddcd9bc6f6dfae0ad7bd
jssong1 <sophia05350@gmail.com>
2021-04-05 18:20:25 +0900

    0405

--- src/App.js
+++ src/App.js
@@ -1,10 +1,101 @@
-import React from 'react';
+import React, { useRef,useState, useMemo, useCallback }  from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
 import InputSample from './InputSample';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중...');
+  return users.filter(user => user.active).length;
+}
+
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username:'',
+    email:''
+  });
+  const {username, email} =inputs;
+  const onChange = useCallback(
+    e =>{
+    const{name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]:value
+    });
+    }, [inputs]
+  );
+
+
+  const [users,setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active : true
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active : false
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active : false
+    }
+  ]);
+
+
+  const nextId = useRef(4);
+  const onCreate= useCallback(
+    () =>{ // 새 배열 추가
+      const user={
+        id: nextId.current,
+        username,
+        email
+      };
+      //setUsers([...users, user]); //spread 연산자
+      setUsers(users.concat(user)); //concat 함수 : 기존배열수정없이 새로운 원소가 추가된 새로운 배열 만들어줌
+
+      setInputs({
+        username:'',
+        email:''
+      });
+      nextId.current +=1;
+    },[users,username,email]
+  );
+  
+
+  const onRemove = useCallback( 
+    id => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users.filter(user => user.id !== id));
+    },
+    [users]
+  );  
+  const onToggle = useCallback(
+    id =>{
+      setUsers(
+        users.map(user=>
+          user.id === id? {...user, active: !user.active} :user 
+        )
+      );
+    },
+    [users]
+  );
+
+/*useMemo 의 첫번째 파라미터에는 어떻게 연산할지 정의하는 함수를 넣어주면 되고 
+두번째 파라미터에는 deps 배열을 넣어주면 되는데, 
+이 배열 안에 넣은 내용이 바뀌면, 우리가 등록한 함수를 호출해서 값을 연산해주고, 
+만약에 내용이 바뀌지 않았다면 이전에 연산한 값을 재사용하게 됩니다.  */
+ const count = useMemo(()=>countActiveUsers(users), [users]);
+
   return (
     <>
     <Wrapper>
@@ -13,6 +104,14 @@ function App() {
     </Wrapper>
     <Counter />
     <InputSample />
+    <CreateUser 
+      username={username}
+      email={email}
+      onChange={onChange}
+      onCreate={onCreate}
+    />
+    <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+    <div>활성사용자 수 : {count}</div>
     </>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,36 +1,63 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com'
-    },
-    {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com'
-    },
-    {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com'
-    }
-  ];
+const User = React.memo(function User({ user, onRemove,onToggle }) {
+  /**
+   * useEffect 를 사용 할 때에는 첫번째 파라미터에는 함수, 두번째 파라미터에는 의존값이 들어있는 배열 (deps)을 넣습니다. 만약에 deps 배열을 비우게 된다면, 컴포넌트가 처음 나타날때에만 useEffect 에 등록한 함수가 호출됩니다.
+     useEffect 에서는 함수를 반환 할 수 있는데 이를 cleanup 함수라고 부릅니다. cleanup 함수는 useEffect 에 대한 뒷정리를 해준다고 이해하시면 되는데요, deps 가 비어있는 경우에는 컴포넌트가 사라질 때 cleanup 함수가 호출됩니다.
+   */
+  useEffect(()=>{
+    //console.log('컴포넌트가 화면에 나타남');
+    console.log('user값이 설정됨');
+    console.log(user);
+    return ()=>{
+      //console.log('컴포넌트가 화면에 사라짐');
+      console.log('user가 바뀌기 전..');
+      console.log(user);
+    };
+  },[user]);
+  return (   
+    <div>
+      <b style={{
+        cursor:'pointer',
+        color:user.active? 'green' :'black'
+      }}
+        onClick={()=>onToggle(user.id)}
+      >
+        {user.username}
+      </b> &nbsp;
+      <span>({user.email}) {user.id}</span>
+      <button onClick={()=>onRemove(user.id) }>삭제</button>
+    </div>
+  );
+});
+
+function UserList({users, onRemove, onToggle}) {
   return (
     <div>
-      <div>
-        <b>{users[0].username}</b> <span>({users[0].email})</span>
-      </div>
-      <div>
-        <b>{users[1].username}</b> <span>({users[1].email})</span>
-      </div>
-      <div>
-        <b>{users[2].username}</b> <span>({users[1].email})</span>
-      </div>
+ {/** <User user={users[0]} />
+      <User user={users[1]} />
+       <User user={users[2]} />  */}
+
+{/**리액트에서 배열을 렌더링 할 때에는 key 라는 props 를 설정해야함.
+ *  key 값은 각 원소들마다 가지고 있는 고유값으로 설정을 해야합니다. 
+ * 지금의 경우엔 id 가 고유 값 */}
+      {users.map(user=> (
+      <User 
+        user={user} 
+        key={user.id} 
+        onRemove={onRemove}
+        onToggle={onToggle}
+      />
+      ))}
+
+
+{/**만약 배열 안의 원소가 가지고 있는 고유한 값이 없다면 
+ * map() 함수를 사용 할 때 설정하는 콜백함수의 두번째 파라미터 index 를 key 로 사용하시면 됩니다. 
+      {users.map((user,index)=> (<User user={user} key={index}/>))}
+    */}
     </div>
   );
 }
+
 
 export default UserList;
